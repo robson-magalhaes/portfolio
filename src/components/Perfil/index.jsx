3a84d27db08a961e3a7b5257d075655c123c9d85
Robson Oliveira Magalhães <robsonom92@hotmail.com>
2024-10-12 18:59:54 -0300

    acertando texto de apresentação

--- src/components/Perfil/index.jsx
+++ src/components/Perfil/index.jsx
@@ -14,7 +14,7 @@ export const Perfil = () => {
                     <h5>Robson Oliveira Magalhães</h5>
                 </C.PerfilTitulo>
                 <C.PerfilDescricao>
-                    Sou um desenvolvedor dedicado em transformar ideias em realidade digital. Com habilidades sólidas em front-end e back-end, estou pronto para enfrentar qualquer desafio. Com um olhar atento aos detalhes e uma abordagem centrada no usuário, estou comprometido em entregar soluções de alta qualidade que atendam às necessidades do cliente.
+                    Sou um desenvolvedor especializado em criar sites e aplicativos personalizados, transformando ideias em soluções digitais eficazes. Com experiência abrangente em front-end e back-end, estou pronto para enfrentar desafios complexos, sempre com foco na experiência do usuário e na eficiência do projeto. Meu objetivo é entregar produtos de alta qualidade, ajustados às necessidades do cliente e com atenção aos mínimos detalhes.
                 </C.PerfilDescricao>
             </C.PerfilApresentacao>
         </C.HeaderPerfil>
